117f673d34e7f2865903b38e702a31b5d905e6f3
Iipin <thwan1357@gmail.com>
2019-09-08 18:15:26 +0200

    Set MEDIA_{URL,ROOT} by default and serve media in debug mode

--- kcc3/urls.py
+++ kcc3/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 from badges.viewsets import BadgeViewSet
 from chombos.viewsets import ChomboViewSet
@@ -31,4 +33,4 @@ urlpatterns = [
     path('api-auth/',
          include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
