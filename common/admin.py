320ccb6330687b998edce293be814e40db9e35c2
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-08 20:18:05 +0200

    Add more features to Player Django Admin

--- common/admin.py
+++ common/admin.py
@@ -4,7 +4,9 @@ from common.models import Player
 
 
 class PlayerAdmin(admin.ModelAdmin):
-    search_fields = ['first_name', 'last_name']
+    search_fields = ('first_name', 'last_name')
+    list_display = ('first_name', 'last_name')
+    list_display_links = ('first_name', 'last_name')
 
 
 admin.site.register(Player, PlayerAdmin)
--- common/models.py
+++ common/models.py
@@ -8,3 +8,6 @@ class Player(models.Model):
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
