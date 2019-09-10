f2202901c5bd9a15d9258a04804cfa09787e69cc
Iipin <thwan1357@gmail.com>
2019-09-10 19:58:10 +0200

    Add USMA ID to Player model

--- common/models.py
+++ common/models.py
@@ -8,6 +8,7 @@ class Player(models.Model):
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
+    usma_id = models.CharField(max_length=30, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
