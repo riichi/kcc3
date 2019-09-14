cc5561b30a409963d682d747b66ce68aac651e3c
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-14 21:30:26 +0200

    Create periodic_task on Badge save

--- badges/models.py
+++ badges/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 from badges.token_generator import generate_token, MAX_TOKEN_LENGTH
 from common.models import Player
@@ -19,11 +20,44 @@ class Badge(models.Model):
     token = models.CharField(
         null=True, max_length=MAX_TOKEN_LENGTH, default=generate_token,
         editable=False)
+    periodic_task = models.OneToOneField(
+        to=PeriodicTask, on_delete=models.CASCADE, null=True, editable=False)
 
     players = models.ManyToManyField(to=Player, blank=True)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__original_refresh_interval = self.refresh_interval
+
     def get_absolute_url(self):
         return reverse('badge-detail', kwargs={'slug': self.id})
+
+    def save(self, *args, **kwargs):
+        if self.refresh_interval != self.__original_refresh_interval:
+            self.__update_task()
+
+        super(Badge, self).save(*args, **kwargs)
+
+    def __update_task(self):
+        if self.refresh_interval is None and self.periodic_task is not None:
+            self.periodic_task.delete()
+        elif self.refresh_interval is not None:
+            self.__create_or_update_task_object()
+
+    def __create_or_update_task_object(self):
+        if self.periodic_task is None:
+            task = PeriodicTask(task='kcc3.celery.debug_task')
+            self.periodic_task = task
+        else:
+            task = self.periodic_task
+
+        schedule, _ = IntervalSchedule.objects.get_or_create(
+            every=round(self.refresh_interval.total_seconds()),
+            period=IntervalSchedule.SECONDS)
+        task.interval = schedule
+        task.name = f'Update {self.id} badge'
+
+        task.save()
 
     def __str__(self):
         return self.title
