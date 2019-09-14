4c0b56f9b78db8c074f3065fc26bd59f8e8df6e6
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-14 20:20:01 +0200

    Add Discord ID to the Player model

--- common/models.py
+++ common/models.py
@@ -9,7 +9,9 @@ class Player(models.Model):
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
-    usma_id = models.CharField(max_length=30, null=True)
+
+    usma_id = models.CharField(max_length=30, null=True, blank=True)
+    discord_id = models.CharField(max_length=30, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse('player-detail', kwargs={'pk': self.pk})
