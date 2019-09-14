a9f7ea58d86d94f79bf8679b12b5111a027e7ad6
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-14 19:57:05 +0200

    Use get_absolute_url() instead of {% url %}

--- common/models.py
+++ common/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Player(models.Model):
@@ -9,6 +10,9 @@ class Player(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
     usma_id = models.CharField(max_length=30, null=True)
+
+    def get_absolute_url(self):
+        return reverse('player-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
