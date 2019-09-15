14256bb5504c4c2f2b7c7311f40db1dd5ee06f68
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-15 17:35:45 +0200

    Add more branding

--- kcc3/settings/base.py
+++ kcc3/settings/base.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'kcc3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,6 +107,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATIC_URL = '/static/'
 
