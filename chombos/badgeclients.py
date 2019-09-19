42b54707933b4630f53caa786eccc26b49265885
Iipin <thwan1357@gmail.com>
2019-09-19 19:24:13 +0200

    Add "in-range" local chombo badge client

--- chombos/badgeclients.py
+++ chombos/badgeclients.py
@@ -1,8 +1,22 @@
 from badges.local_badge_client import LocalBadgeClient, PlayerIterable
 from badgeupdater.models import BadgeUpdateRequest
 from chombos.models import Chombo
+from players.models import Player
 
 
 class ChombosBadgeClient(LocalBadgeClient):
     def get_badge_players(self, request: BadgeUpdateRequest) -> PlayerIterable:
         return Chombo.objects.values_list('player')
+
+
+class ChombosInRangeClient(LocalBadgeClient):
+    def get_badge_players(self, request: BadgeUpdateRequest) -> PlayerIterable:
+        a = int(self.request.query_params.get('a'))
+        b = self.request.query_params.get('b')
+        if b is None:
+            pred = lambda c: a <= c
+        else:
+            b = int(b)
+            pred = lambda c: a <= c <= b
+        return filter(lambda p: pred(p.chombo_set.count()),
+                      Player.objects.all())
--- chombos/urls.py
+++ chombos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from chombos.badgeclients import ChombosBadgeClient
+from chombos.badgeclients import ChombosBadgeClient, ChombosInRangeClient
 
 badgeclients_urlpatterns = [
     path('chombos/', ChombosBadgeClient.as_view()),
+    path('in-range/', ChombosInRangeClient.as_view()),
 ]
