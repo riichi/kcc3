ba349717e9bd38beb32b645786582588779cd539
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-20 12:16:55 +0200

    Add nickname to Player model

--- players/admin.py
+++ players/admin.py
@@ -4,9 +4,11 @@ from players.models import Player
 
 
 class PlayerAdmin(admin.ModelAdmin):
-    search_fields = ('user', 'first_name', 'last_name', 'usma_id', 'discord_id')
-    list_display = ('user', 'first_name', 'last_name', 'usma_id', 'discord_id')
-    list_display_links = ('first_name', 'last_name')
+    search_fields = (
+        'user', 'first_name', 'last_name', 'nickname', 'usma_id', 'discord_id')
+    list_display = (
+        'user', 'first_name', 'last_name', 'nickname', 'usma_id', 'discord_id')
+    list_display_links = ('first_name', 'last_name', 'nickname')
 
 
 admin.site.register(Player, PlayerAdmin)
--- players/models.py
+++ players/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 
@@ -7,16 +8,36 @@ class Player(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.SET_NULL, null=True, blank=True)
 
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=150, null=True, blank=True)
+    nickname = models.CharField(max_length=50, null=True, blank=True)
 
     usma_id = models.CharField(
         max_length=30, null=True, blank=True, verbose_name='USMA ID')
     discord_id = models.CharField(
         max_length=30, null=True, blank=True, verbose_name='Discord ID')
 
+    def clean(self):
+        has_name = self.first_name and self.last_name
+        has_nickname = self.nickname
+
+        if not (has_name or has_nickname):
+            raise ValidationError(
+                {'first_name': 'Either full name, or nickname is required'})
+
     def get_absolute_url(self):
         return reverse('player-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        s = ''
+
+        if self.first_name and self.last_name:
+            s = f'{self.first_name} {self.last_name}'
+
+        if self.nickname:
+            if s:
+                s += f' ({self.nickname})'
+            else:
+                s = self.nickname
+
+        return s
