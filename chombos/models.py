17285542651d91142564ae867f9c95280c17d8cd
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-20 12:15:59 +0200

    Make chombo comment not required

--- chombos/models.py
+++ chombos/models.py
@@ -6,7 +6,7 @@ from players.models import Player
 class Chombo(models.Model):
     player = models.ForeignKey(to=Player, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(db_index=True)
-    comment = models.TextField()
+    comment = models.TextField(blank=True)
 
     def __str__(self):
         return f'{self.player} at {self.timestamp}'
