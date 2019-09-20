e7c36e8aa1196dcae15a630f60d1ff1b67c9e004
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-20 13:43:05 +0200

    Remove searching Player by User which was throwing an error

--- players/admin.py
+++ players/admin.py
@@ -5,7 +5,7 @@ from players.models import Player
 
 class PlayerAdmin(admin.ModelAdmin):
     search_fields = (
-        'user', 'first_name', 'last_name', 'nickname', 'usma_id', 'discord_id')
+        'first_name', 'last_name', 'nickname', 'usma_id', 'discord_id')
     list_display = (
         'user', 'first_name', 'last_name', 'nickname', 'usma_id', 'discord_id')
     list_display_links = ('first_name', 'last_name', 'nickname')
