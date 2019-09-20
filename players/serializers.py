9775f8bca52c9814f208f1c3bc3c4a1aa2d30d23
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-21 01:52:35 +0200

    Serialize player's ID, Discord ID and USMA ID

--- players/serializers.py
+++ players/serializers.py
@@ -6,4 +6,7 @@ from .models import Player
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = ['first_name', 'last_name', 'badge_set']
+        fields = [
+            'id', 'first_name', 'last_name', 'badge_set', 'usma_id',
+            'discord_id'
+        ]
