d5722281b3ae689e56ffd924d067d6666e7c769d
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-21 01:50:37 +0200

    Set default API permissions to IsAuthenticatedOrReadOnly

--- kcc3/settings/base.py
+++ kcc3/settings/base.py
@@ -121,7 +121,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
 }
 
 
