3968218545b9ee60909fd641840289f041c75789
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-09-30 10:18:28 +0200

    Add Yakuman details template

--- yakumans/urls.py
+++ yakumans/urls.py
@@ -1,4 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
+from yakumans.views import YakumanDetailView
+
 urlpatterns = [
-]
+    path('<int:pk>/', YakumanDetailView.as_view(), name='yakuman-detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- yakumans/views.py
+++ yakumans/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from yakumans.models import Yakuman
+
+
+class YakumanDetailView(DetailView):
+    model = Yakuman
+    slug_field = 'id'
