d3c5071ea920f387d19910d9082b903281f23697
Iipin <52832022+Iipin@users.noreply.github.com>
2026-02-16 20:41:27 +0100

    validate weight

--- chombos/models.py
+++ chombos/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from players.models import Player
@@ -7,7 +8,9 @@ class Chombo(models.Model):
     player = models.ForeignKey(to=Player, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(db_index=True)
     comment = models.TextField(blank=True)
-    weight = models.DecimalField(max_digits=3, decimal_places=1, default=1)
+    weight = models.DecimalField(
+        max_digits=3, decimal_places=1, default=1, validators=[MinValueValidator(1), MaxValueValidator(6)]
+    )
 
     def __str__(self):
         return f"{self.player} at {self.timestamp}"
